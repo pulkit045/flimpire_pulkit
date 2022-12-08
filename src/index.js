2506ac92c884ef5f6581681d25e2a69411a67b4e
Pulkit Gupta <pulkitgupta045@gmail.com>
2022-12-08 22:54:57 +0530

    completed desgin for navbar and sidebar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const theme = createTheme({});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
